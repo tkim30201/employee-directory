da38a85f476e51b4ff7f55395c5ea78dc9afa3ff
tkim30201 <63823948+tkim30201@users.noreply.github.com>
2020-09-29 15:49:49 -0400

    added to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Search from "./components/Search";
+import List from "./components/List";
+import employeeData from './utils/employeeData.json';
 import { Component } from 'react';
 
-
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: employeeData, searchUser: '' };
+  }
+  handleInput = e => {
+    console.log(e.target.value);
+    this.setState({ searchUser: e.target.value });
+  };
+  render() {
+    let filteredUsers = this.state.users.filter(user => {
+      return user.name
+        .toLowerCase()
+        .includes(this.state.searchUser.toLowerCase());
+    });
+    return (
+      <div className="App">
+        <Header />
+        <Search handleInput={this.handleInput} />
+        <List filteredUsers={filteredUsers} />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default App;
